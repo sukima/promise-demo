645856d2ba173df9f411c8635aa860ebe9777aa8
Devin Weaver <suki@tritarget.org>
2013-11-14 20:12:53 -0500

    Add random timeout
    Will choose a timeout between 100 and 5000 as multipes of 10

--- data_generator.js
+++ data_generator.js
@@ -14,10 +14,14 @@ exports.buildData = function (size) {
   for (i = 0; i < size; i++) {
     dataObject = new DataObject({
       id:      i,
-      timeout: 100,
+      timeout: randomTimeout(100, 5000),
       title:   "foobar"
     });
     storage.push(dataObject);
   }
   return storage;
 };
+
+function randomTimeout(min, max) {
+  return (Math.floor((Math.random() * (max - min) + min) / 10) * 10);
+}
