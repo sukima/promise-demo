6915cf5de6ee10571728e58c22d3b90ce45582c7
Devin Weaver <suki@tritarget.org>
2013-11-17 14:19:52 -0500

    Remove dead code :skull:

--- src/app.js
+++ src/app.js
@@ -2,14 +2,8 @@ var Q = require("q");
 var $ = require("jquery");
 var DataGenerator = require("./data_generator");
 
-var promisesArray = DataGenerator.buildData(10000);
 var start_time, promises_array, numOfTasksComplete;
 var number_of_objects = 10000; // Is that a lot? 😱
-
-var i, len, start_time = new Date().getTime();
-for(i = 0, len = promisesArray.length; i < len; i++) {
-  promisesArray[i].then(logDataObject);
-}
 
 function init() {
   numOfTasksComplete = 0;
@@ -23,20 +17,6 @@ function init() {
     $("#info").text("Done. " + promises_array.length + " objects processed in " + time + " ms.");
   }).done();
 }
-
-// TODO: This should be allSettled and then loop through the results to
-// determine pass or fail. This will be needed when the visual aspect of this
-// project will use fulfilled and rejected states to manage the DOM.
-Q.all(promisesArray).then(function(resolved) {
-  console.log("All Done! 😄");
-  console.log("" + resolved.length + " objects processed");
-}, function(reason) {
-  console.log("Something went wrong 😡");
-  console.log(reason);
-}).fin(function() {
-  var time = new Date().getTime() - start_time;
-  console.log("I only took " + time + " ms total!");
-});
 
 function calculateTime() {
   return (new Date().getTime()) - start_time;
@@ -57,15 +37,10 @@ function attachCallback(index, promise) {
   promise.then(fillListItem).done();
 }
 
-function logDataObject(data)  {
-  console.log(data.toString());
-}
-
 function fillListItem(data) {
   numOfTasksComplete++;
   $("#count").text("" + numOfTasksComplete + " (" + calculateTime() + " ms)");
   $("#item-" + data.id).text(data.toString());
 }
 
-console.log("Here we go!");
 $(init);
