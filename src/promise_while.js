6d405f7af2c9cf05e77e63c402bdb5cf97d56b79
Devin Weaver <suki@tritarget.org>
2013-11-17 16:19:26 -0500

    Add promiseWhile module
    Used to allow a loop to be non-blocking using promises.

---
+++ src/promise_while.js
@@ -0,0 +1,32 @@
+// PromiseWhile - an extention to do for loops in a non-blocking way
+//
+// Taken from http://stackoverflow.com/a/17238793/227176
+//
+Q = require("q");
+
+// `condition` is a function that returns a boolean
+// `body` is a function that returns a promise
+// returns a promise for the completion of the loop
+function promiseWhile(condition, body) {
+    var done = Q.defer();
+
+    function loop() {
+        // When the result of calling `condition` is no longer true, we are
+        // done.
+        if (!condition()) return done.resolve();
+        // Use `when`, in case `body` does not return a promise.
+        // When it completes loop again otherwise, if it fails, reject the
+        // done promise
+        Q.when(body(), loop, done.reject);
+    }
+
+    // Start running the loop in the next tick so that this function is
+    // completely async. It would be unexpected if `body` was called
+    // synchronously the first time.
+    Q.nextTick(loop);
+
+    // The promise
+    return done.promise;
+}
+
+module.exports = promiseWhile;
