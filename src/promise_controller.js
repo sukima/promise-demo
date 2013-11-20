610bc846e2acd8c7bfa4c6497f311b6e68a79de6
Devin Weaver <suki@tritarget.org>
2013-11-20 16:43:20 -0500

    Replace confirm with ConfirmationController

--- src/promise_controller.js
+++ src/promise_controller.js
@@ -3,6 +3,7 @@ var Q = require("q");
 var $ = require("jquery");
 var DataGenerator = require("./data_generator");
 var promiseWhile = require("./promise_while");
+var ConfirmationController = require("./confirmation_controller");
 
 // PromiseController {{{1
 function PromiseController() {
@@ -120,16 +121,21 @@ PromiseController.prototype.finish = function finish() {
 // PromiseController::validateDataSize {{{1
 PromiseController.prototype.validateDataSize = function validateDataSize() {
   var size = this.dataSetSize();
+  var confirmation = ConfirmationController.getInstance();
+  var message;
   if (size >= 90000) {
-    return promiseConfirmation("Gulp! I really think that is too high. Blue smoke will probubly come out the back of your computer! Are you absolutly sure?");
+    message = "Gulp! I really think that is too high. Blue smoke will probubly come out the back of your computer! Are you absolutly sure?";
   }
-  if (size >= 50000) {
-    return promiseConfirmation("Whoa Nelly! That's a lot! The browser will probubly cry. Are you really sure you want to do this?");
+  else if (size >= 50000) {
+    message = "Whoa Nelly! That's a lot! The browser will probubly cry. Are you really sure you want to do this?";
   }
-  if (size >= 10000) {
-    return promiseConfirmation("That's getting a little heavy. Are you sure you want to bog your browser down?");
+  else if (size >= 10000) {
+    message = "That's getting a little heavy. Are you sure you want to bog your browser down?";
   }
-  return Q("Data size ok");
+  else {
+    return Q("Data size ok");
+  }
+  return confirmation.open(message).promise();
 };
 
 
@@ -249,18 +255,6 @@ PromiseController.prototype.calculateTime = function calculateTime() {
   var current_time = this.end_time || new Date().getTime();
   return current_time - this.start_time;
 };
-
-
-// Helper functions {{{1
-// promiseConfirmation {{{2
-function promiseConfirmation(message) {
-  return Q.delay(1).then(function() {
-    if (confirm(message)) {
-      return "User confirmed";
-    }
-    throw "User cancled";
-  });
-}
 // }}}1
 
 module.exports = PromiseController;
