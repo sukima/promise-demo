f160f08a17594ffdc011b2ff1b26dcd44e66a4ae
Devin Weaver <suki@tritarget.org>
2013-11-21 09:09:06 -0500

    Add an about box when logo is clicked

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 var $ = require("jquery");
+var jq_alert = require("./confirmation_controller").alert;
 var PromiseController = require("./promise_controller");
+
+var version = "0.0.1";
 
 require("jquery-ui");
 
 $(function() {
   var appController = new PromiseController();
   appController.init();
+  $("#logo").click(showAbout);
 });
+
+function showAbout() {
+  jq_alert("Promise-Demo project, version " + version, "About");
+}
