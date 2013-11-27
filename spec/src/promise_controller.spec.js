e254d4e64c2b28fcb84899c87fbfd70f3b056b1e
Devin Weaver <suki@tritarget.org>
2013-11-26 23:19:50 -0500

    Whitespace fix

--- spec/src/promise_controller.spec.js
+++ spec/src/promise_controller.spec.js
@@ -1,63 +1,64 @@
 describe("PromiseController", function() {
-	var PromiseController = require("../../src/promise_controller");
+  var PromiseController = require("../../src/promise_controller");
 
-	beforeEach(function() {
-		loadFixtures("promise_controller.html");
-		this.pc = new PromiseController();
-	});
+  beforeEach(function() {
+    loadFixtures("promise_controller.html");
+    this.pc = new PromiseController();
+  });
 
-	describe("#init", function() {
+  describe("#init", function() {
 
-		it("should be chainable", function() {
-			expect( this.pc.init() ).toBe(this.pc);
-		});
+    it("should be chainable", function() {
+      expect( this.pc.init() ).toBe(this.pc);
+    });
 
-	});
+  });
 
-	describe("#getAllowFailures", function() {
+  describe("#getAllowFailures", function() {
 
-		it("should return false when checkbox is unchecked", function() {
-			expect( this.pc.getAllowFailures() ).toBeFalsy();
-		});
+    it("should return false when checkbox is unchecked", function() {
+      expect( this.pc.getAllowFailures() ).toBeFalsy();
+    });
 
-		it("should return true when checkbox is checked", function() {
-			$("#allow-failures").prop("checked", true);
-			expect( this.pc.getAllowFailures() ).toBeTruthy();
-		});
+    it("should return true when checkbox is checked", function() {
+      $("#allow-failures").prop("checked", true);
+      expect( this.pc.getAllowFailures() ).toBeTruthy();
+    });
 
-	});
+  });
 
-	describe("#getDataSetSize", function() {
+  describe("#getDataSetSize", function() {
 
-		beforeEach(function() {
-			this.input = $("#data-size");
-		});
+    beforeEach(function() {
+      this.input = $("#data-size");
+    });
 
-		it("should return the value from the input field", function() {
-			this.input.val(0);
-			expect( this.pc.getDataSetSize() ).toBe(0);
-			this.input.val(12);
-			expect( this.pc.getDataSetSize() ).toBe(12);
-			this.input.val(123);
-			expect( this.pc.getDataSetSize() ).toBe(123);
-		});
+    it("should return the value from the input field", function() {
+      this.input.val(0);
+      expect( this.pc.getDataSetSize() ).toBe(0);
+      this.input.val(12);
+      expect( this.pc.getDataSetSize() ).toBe(12);
+      this.input.val(123);
+      expect( this.pc.getDataSetSize() ).toBe(123);
+    });
 
-		it("should return 0 with invalid input", function() {
-			this.input.val("foo");
-			expect( this.pc.getDataSetSize() ).toBe(0);
-		});
+    it("should return 0 with invalid input", function() {
+      this.input.val("foo");
+      expect( this.pc.getDataSetSize() ).toBe(0);
+    });
 
-	});
+  });
 
-	describe("When the run button is pressed", function() {
+  describe("When the run button is pressed", function() {
 
-		it("should run start()", function() {
-			spyOn(this.pc, "start");
-			expect( this.pc.start ).not.toHaveBeenCalled();
-			$("#run-btn").trigger("click");
-			expect( this.pc.start ).toHaveBeenCalled();
-		});
+    it("should run start()", function() {
+      spyOn(this.pc, "start");
+      expect( this.pc.start ).not.toHaveBeenCalled();
+      $("#run-btn").trigger("click");
+      expect( this.pc.start ).toHaveBeenCalled();
+    });
 
-	});
+  });
 
 });
+/* vim:set ts=2 sw=2 et: */
