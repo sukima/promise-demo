45f0b1599efa5c3411a726d48bc2113e315a66a8
Devin Weaver <suki@tritarget.org>
2013-11-29 19:07:37 -0500

    Whitespace fixes

--- spec/src/data_generator.spec.js
+++ spec/src/data_generator.spec.js
@@ -1,119 +1,120 @@
 describe("DataGenerator", function() {
-	var DataGenerator = require("../../src/data_generator");
+  var DataGenerator = require("../../src/data_generator");
 
-	describe("DataObject", function() {
+  describe("DataObject", function() {
 
-		beforeEach(function() {
-			this.test_obj = new DataGenerator({id: 0, title: "foobar"});
-		});
+    beforeEach(function() {
+      this.test_obj = new DataGenerator({id: 0, title: "foobar"});
+    });
 
-		describe("#constructor", function() {
+    describe("#constructor", function() {
 
-			it("should set a created_on timestamp", function() {
-				expect( this.test_obj.created_on ).toBeDefined();
-			});
+      it("should set a created_on timestamp", function() {
+        expect( this.test_obj.created_on ).toBeDefined();
+      });
 
-			it("should throw an exception if arguments are missing", function() {
-				expect(function() { new DataGenerator({}); }).toThrow();
-				expect(function() { new DataGenerator({ id: 1 }); }).toThrow();
-				expect(function() { new DataGenerator({ title: "bar" }); }).toThrow();
-			});
+      it("should throw an exception if arguments are missing", function() {
+        expect(function() { new DataGenerator({}); }).toThrow();
+        expect(function() { new DataGenerator({ id: 1 }); }).toThrow();
+        expect(function() { new DataGenerator({ title: "bar" }); }).toThrow();
+      });
 
-		});
+    });
 
-		describe("#getRunningTime", function() {
+    describe("#getRunningTime", function() {
 
-			it("should return -1 when object has not completed yet", function() {
-				expect( this.test_obj.getRunningTime() ).toBe(-1);
-			});
+      it("should return -1 when object has not completed yet", function() {
+        expect( this.test_obj.getRunningTime() ).toBe(-1);
+      });
 
-			it("should return a number", function() {
-				this.test_obj.completed_on = new Date().getTime() + 1000;
-				expect( this.test_obj.getRunningTime() ).toEqual(jasmine.any(Number));
-			});
+      it("should return a number", function() {
+        this.test_obj.completed_on = new Date().getTime() + 1000;
+        expect( this.test_obj.getRunningTime() ).toEqual(jasmine.any(Number));
+      });
 
-		});
+    });
 
-		describe("#start", function() {
+    describe("#start", function() {
 
-			beforeEach(function() {
-				this.test_obj.timeout = 1;
-			});
+      beforeEach(function() {
+        this.test_obj.timeout = 1;
+      });
 
-			it("should return a promise", function() {
-				expect( this.test_obj.start().then ).toBeDefined();
-			});
+      it("should return a promise", function() {
+        expect( this.test_obj.start().then ).toBeDefined();
+      });
 
-			it("should assign a completed_on when resolved", function() {
-				var _this = this;
-				runs(function() {
-					return this.test_obj.start().then(
-						function() {
-							expect( _this.test_obj.completed_on ).toEqual(jasmine.any(Number));
-						},
-						jasmine.expectedFulfilled
-					);
-				});
-			});
+      it("should assign a completed_on when resolved", function() {
+        var _this = this;
+        runs(function() {
+          return this.test_obj.start().then(
+            function() {
+              expect( _this.test_obj.completed_on ).toEqual(jasmine.any(Number));
+            },
+            jasmine.expectedFulfilled
+          );
+        });
+      });
 
-			it("should provide itself as the value to the fulfilled promise", function() {
-				var _this = this;
-				runs(function() {
-					return this.test_obj.start().then(
-						function(value) {
-							expect( value ).toBe(_this.test_obj);
-						},
-						jasmine.expectedFulfilled
-					);
-				});
-			});
+      it("should provide itself as the value to the fulfilled promise", function() {
+        var _this = this;
+        runs(function() {
+          return this.test_obj.start().then(
+            function(value) {
+              expect( value ).toBe(_this.test_obj);
+            },
+            jasmine.expectedFulfilled
+          );
+        });
+      });
 
-			it("should provide itself as the value to the rejected promise", function() {
-				this.test_obj.isABadWorker = true;
-				var _this = this;
-				runs(function() {
-					return this.test_obj.start().then(
-						jasmine.expectedRejected,
-						function(reason) {
-							expect( reason ).toBe(_this.test_obj);
-						}
-					);
-				});
-			});
+      it("should provide itself as the value to the rejected promise", function() {
+        this.test_obj.isABadWorker = true;
+        var _this = this;
+        runs(function() {
+          return this.test_obj.start().then(
+            jasmine.expectedRejected,
+            function(reason) {
+              expect( reason ).toBe(_this.test_obj);
+            }
+          );
+        });
+      });
 
-		});
+    });
 
-		describe("#toString", function() {
+    describe("#toString", function() {
 
-			it("should return a string", function() {
-				expect( this.test_obj.toString() ).toEqual(jasmine.any(String));
-			});
+      it("should return a string", function() {
+        expect( this.test_obj.toString() ).toEqual(jasmine.any(String));
+      });
 
-		});
+    });
 
-	});
+  });
 
-	describe("#buildData (static)", function() {
+  describe("#buildData (static)", function() {
 
-		it("should return a promise", function() {
-			expect( DataGenerator.buildData(1).then ).toBeDefined();
-		});
+    it("should return a promise", function() {
+      expect( DataGenerator.buildData(1).then ).toBeDefined();
+    });
 
-		it("should create an array of data objects", function() {
-			runs(function() {
-				return DataGenerator.buildData(3).then(
-					function(value) {
-						expect( value ).toEqual([
-							jasmine.any(DataGenerator),
-							jasmine.any(DataGenerator),
-							jasmine.any(DataGenerator)
-						]);
-					},
-					jasmine.expectFulfilled
-				);
-			});
-		});
+    it("should create an array of data objects", function() {
+      runs(function() {
+        return DataGenerator.buildData(3).then(
+          function(value) {
+            expect( value ).toEqual([
+              jasmine.any(DataGenerator),
+              jasmine.any(DataGenerator),
+              jasmine.any(DataGenerator)
+            ]);
+          },
+          jasmine.expectFulfilled
+        );
+      });
+    });
 
-	});
+  });
 
 });
+/* vim:set sw=2 ts=2 et fdm=marker: */
