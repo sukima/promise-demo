da9a2a610bd8f083efbb5c1aa851920703ef2357
Devin Weaver <suki@tritarget.org>
2013-12-04 14:21:31 -0500

    Add specs (skeletons) for race_cotroller. (WIP)

--- spec/src/race_controller.spec.js
+++ spec/src/race_controller.spec.js
@@ -1,6 +1,59 @@
 describe("RaceController", function() {
   var RaceController = require("../../src/race_controller");
 
-  describe("description", function() {
+  beforeEach(function() {
+    this.rc = new RaceController();
+  });
+
+  describe("#constructor", function() {
+  });
+
+  describe("#init", function() {
+
+    it("should be chainable", function() {
+      expect( this.rc.init() ).toBe(this.rc);
+    });
+
+  });
+
+  describe("#start", function() {
+  });
+
+  describe("#enableControls", function() {
+
+    it("should disable the buttons", function() {
+      spyOn($.fn, "button");
+      this.rc.enableControls();
+      expect( $.fn.button ).toHaveBeenCalledWith("enable");
+    });
+
+  });
+
+  describe("#disableControls", function() {
+
+    it("should disable the buttons", function() {
+      spyOn($.fn, "button");
+      this.rc.disableControls();
+      expect( $.fn.button ).toHaveBeenCalledWith("disable");
+    });
+
+  });
+
+  describe("#readyHorses", function() {
+  });
+
+  describe("#setHorses", function() {
+  });
+
+  describe("#goHorses", function() {
+  });
+
+  describe("#trotHorses", function() {
+  });
+
+  describe("#finishRace", function() {
+  });
+
+  describe("#reset", function() {
   });
 });
